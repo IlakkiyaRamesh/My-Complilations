09cfbcefd4f507a14b75486533ee6379e70976ce
ilakkiya <ilakkiyarameshh@gmail.com>
2018-09-12 22:15:40 +0530

    12-09-2018

--- java/PrintEvenFromArrayInput.java
+++ java/PrintEvenFromArrayInput.java
@@ -20,7 +20,12 @@ public class PrintEvenFromArrayInput{
                     System.out.print(a[i]);
                 }
             }
-        }
- 
-    }
+        }System.out.println();
+        int sum=0;
+        for(i=0;i<size;i++){
+            if(a[i]%2!=0){
+                sum+=a[i];
+            }
+        }System.out.println("Sum of Odd Numbers:"+sum);
+    }  
 }
